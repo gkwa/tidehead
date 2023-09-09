b4d58262c1725e9169a7ff0ef0043b64d4fd85b9
Taylor Monacelli <taylormonacelli@gmail.com>
2023-09-09 10:08:34 -0700

    refactor

--- tidehead.go
+++ tidehead.go
@@ -6,20 +6,20 @@ import (
 	"time"
 )
 
-func FormatDuration(d1 time.Duration) string {
-	const daysInMonth = 30 // Approximate value
+func FormatDuration(d time.Duration) string {
+	const daysInMonth = 30 // Approximate
 
-	years := d1 / (365 * 24 * time.Hour)
-	d1 -= years * 365 * 24 * time.Hour
-	months := d1 / (daysInMonth * 24 * time.Hour)
-	d1 -= months * daysInMonth * 24 * time.Hour
-	days := d1 / (24 * time.Hour)
-	d1 -= days * 24 * time.Hour
-	hours := d1 / time.Hour
-	d1 -= hours * time.Hour
-	minutes := d1 / time.Minute
-	d1 -= minutes * time.Minute
-	seconds := d1 / time.Second
+	years := d / (365 * 24 * time.Hour)
+	d -= years * 365 * 24 * time.Hour
+	months := d / (daysInMonth * 24 * time.Hour)
+	d -= months * daysInMonth * 24 * time.Hour
+	days := d / (24 * time.Hour)
+	d -= days * 24 * time.Hour
+	hours := d / time.Hour
+	d -= hours * time.Hour
+	minutes := d / time.Minute
+	d -= minutes * time.Minute
+	seconds := d / time.Second
 
 	x := fmt.Sprintf("y: %d, mo: %d, d: %d, h: %d, m: %d, s: %d\n", years, months, days, hours, minutes, seconds)
 	log.Debug(x)
